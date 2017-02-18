c1a75da175bcbac245e76a1dee9c1070cc902e8c
Wäinö Kotilainen <waino.kotilainen@iki.fi>
2017-02-18 17:39:11 +0200

    add package installation script

---
+++ packages.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+set -euo pipefail
+
+packages=(
+    abcde
+    agave
+    apparmor
+    apparmor-profiles
+    apparmor-profiles-extra
+    apparmor-utils
+    apt-listbugs
+    aptitude
+    archivemail
+    ardour
+    aspell
+    aspell-en
+    audacity
+    bc
+    binwalk
+    blender
+    build-essential
+    byobu
+    ccache
+    chromium-browser
+    clang
+    clang-format
+    clang-tidy
+    cmake
+    cppcheck
+    darktable
+    debian-goodies
+    dia
+    figlet
+    flac
+    fonts-inconsolata
+    fonts-noto
+    gimp
+    git
+    git-annex
+    gitg
+    gitk
+    htop
+    inkscape
+    iotop
+    kcachegrind
+    libboost-all-dev
+    libvirt-bin
+    mercurial
+    mpv
+    mtr
+    netcat-openbsd
+    ninja-build
+    octave
+    okular
+    pdftk
+    pngcrush
+    pv
+    python-pip
+    python-virtualenv
+    qemu-kvm
+    qlandkartegt
+    quodlibet
+    redshift
+    shellcheck
+    sloccount
+    socat
+    stellarium
+    stow
+    subversion
+    tcpdump
+    terminator
+    texlive-full
+    tig
+    tmux
+    tree
+    units
+    upx-ucl
+    valgrind
+    vim
+    virt-manager
+    vlc
+    vorbis-tools
+    whois
+    wireshark
+    x264
+    yasm
+)
+
+
+apt update
+
+apt install --yes "${packages[@]}"
