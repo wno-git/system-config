dac97cf3ff44714521e7d7af32773ebfd1ca7a17
Wäinö Kotilainen <waino.kotilainen@iki.fi>
2017-08-25 20:51:02 +0300

    add more devbox packages

--- packages-devbox.sh
+++ packages-devbox.sh
@@ -3,11 +3,41 @@
 set -euo pipefail
 
 packages=(
+    apt-listbugs
+    bc
     build-essential
+    ccache
+    clang
+    clang-format
+    clang-tidy
+    cmake
+    cppcheck
+    dnsutils
+    gcc-doc
+    gdb-doc
     git
+    glew-utils
     htop
+    libboost-all-dev
+    libgl1-mesa-glx-dbgsym
+    libglew-dev
+    libsdl2-2.0-0-dbgsym
+    libsdl2-dev
+    libsdl2-doc
+    libuv1-dev
+    ninja-build
+    python-pip
+    python-virtualenv
+    shellcheck
+    sloccount
     stow
+    strace
     tig
+    time
+    tmux
+    tree
+    upx-ucl
+    valgrind
     vim
 )
 
